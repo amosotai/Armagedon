3562122ca01d0eb552e66918af70425bb2be59ef
Xea Tinction <145623868+amosotai@users.noreply.github.com>
2024-02-03 02:03:56 +0300

    Add files via upload

---
+++ AI (1).py
@@ -0,0 +1,4 @@
+#My Computer Program
+name = (input('State Your Name: '))
+print('hello,' +name+ ' and welcome to AOS Intro')
+
---
+++ code.py
@@ -0,0 +1,3 @@
+print('Temperature converter')
+temp = eval(input('Enter a temperature in Celsius: '))
+print('In Fahrenheit, that is', 9/5*temp+32)
