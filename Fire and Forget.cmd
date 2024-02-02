71b64dc5535193d39e2334f6b855cfeff5a04b82
Xea Tinction <145623868+amosotai@users.noreply.github.com>
2024-02-03 02:05:28 +0300

    Add files via upload

---
+++ Fire and Forget.cmd
@@ -0,0 +1,17 @@
+@echo
+cls
+color 2
+cls
+echo                                               Fire and Firget Execution protocal                                       
+pause
+cd /
+cd Program Files (x86)
+cd GameTop.com
+cd Fire and Forget
+gtlauncher.exe
+cls
+color 3
+cls
+echo Execution Complete
+pause
+exit
